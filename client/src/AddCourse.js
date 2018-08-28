284b880d504fad11b9914308b75c1118d75eee6a
Haider Malik <haidermalik504@gmail.com>
2018-08-28 15:24:30 +0500

    course created

--- client/src/AddCourse.js
+++ client/src/AddCourse.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
 import './AddCourse.css';
+import { gql } from 'apollo-boost';
+import { Mutation } from 'react-apollo';
+
+const CREATE_COURSE_MUTATION = gql`
+  mutation CreateCourse($name: String!, $description: String!) {
+    createCourse(name: $name, description: $description) {
+      id
+      name
+      description
+      isPublished
+    }
+  }
+`;
 class AddCourse extends Component {
   state = {
     name: '',
@@ -10,48 +23,58 @@ class AddCourse extends Component {
     const value = event.target.value;
     this.setState({ [name]: value });
   };
-  submitCourse = e => {
+  submitCourse = (createCourse, e) => {
     e.preventDefault();
+    createCourse({
+      variables: {
+        name: this.state.name,
+        description: this.state.description
+      }
+    });
     console.log('submitted');
   };
   render() {
     return (
-      <div className="conatiner center_div">
-        <div className="card">
-          <div className="card-title">
-            <h3>Add Course</h3>
-          </div>
-          <div className="card-body">
-            <form onSubmit={this.submitCourse}>
-              <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input
-                  name="name"
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter name"
-                  value={this.state.name}
-                  onChange={this.onChangeHandler}
-                />
+      <Mutation mutation={CREATE_COURSE_MUTATION}>
+        {(createCourse, { data }) => (
+          <div className="conatiner center_div">
+            <div className="card">
+              <div className="card-title">
+                <h3>Add Course</h3>
               </div>
-              <div className="form-group">
-                <label htmlFor="description">Description</label>
-                <textarea
-                  name="description"
-                  className="form-control"
-                  rows="3"
-                  value={this.state.description}
-                  onChange={this.onChangeHandler}
-                />
-              </div>
+              <div className="card-body">
+                <form onSubmit={this.submitCourse.bind(this, createCourse)}>
+                  <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input
+                      name="name"
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter name"
+                      value={this.state.name}
+                      onChange={this.onChangeHandler}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="description">Description</label>
+                    <textarea
+                      name="description"
+                      className="form-control"
+                      rows="3"
+                      value={this.state.description}
+                      onChange={this.onChangeHandler}
+                    />
+                  </div>
 
-              <button type="submit" className="btn btn-primary btn-block ">
-                Save
-              </button>
-            </form>
+                  <button type="submit" className="btn btn-primary btn-block ">
+                    Save
+                  </button>
+                </form>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        )}
+      </Mutation>
     );
   }
 }
