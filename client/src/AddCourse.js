d52cf1b805c1a9d15203e78018cfb155b7e18633
Haider Malik <haidermalik504@gmail.com>
2018-08-28 17:52:09 +0500

    create query

--- client/src/AddCourse.js
+++ client/src/AddCourse.js
@@ -3,24 +3,6 @@ import './AddCourse.css';
 import { gql } from 'apollo-boost';
 import { Mutation } from 'react-apollo';
 
-const CREATE_COURSE_MUTATION = gql`
-  mutation CreateCourse($name: String!, $description: String!) {
-    createCourse(name: $name, description: $description) {
-      id
-      name
-      description
-      isPublished
-    }
-  }
-`;
-// const COURSE_FEED_QUERY = gql`
-// courseFeed {
-//     id
-//     name
-//     description
-//     isPublished
-// }
-// `
 const COURSE_FEED_QUERY = gql`
   {
     courseFeed {
@@ -41,9 +23,9 @@ class AddCourse extends Component {
     const value = event.target.value;
     this.setState({ [name]: value });
   };
-  submitCourse = (createCourse, e) => {
+  submitCourse = async (createCourse, e) => {
     e.preventDefault();
-    createCourse({
+    await createCourse({
       variables: {
         name: this.state.name,
         description: this.state.description
@@ -51,6 +33,44 @@ class AddCourse extends Component {
     });
   };
   render() {
+    const renderCreateCourse = (createCourse, { data, error, loading }) => (
+      <div className="conatiner center_div">
+        <div className="card">
+          <div className="card-title">
+            <h3>Add Course</h3>
+          </div>
+          <div className="card-body">
+            <form onSubmit={this.submitCourse.bind(this, createCourse)}>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input
+                  name="name"
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter name"
+                  value={this.state.name}
+                  onChange={this.onChangeHandler}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="description">Description</label>
+                <textarea
+                  name="description"
+                  className="form-control"
+                  rows="3"
+                  value={this.state.description}
+                  onChange={this.onChangeHandler}
+                />
+              </div>
+
+              <button type="submit" className="btn btn-primary btn-block ">
+                Save
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
     return (
       <Mutation
         mutation={CREATE_COURSE_MUTATION}
@@ -64,46 +84,19 @@ class AddCourse extends Component {
           });
         }}
       >
-        {(createCourse, { data }) => (
-          <div className="conatiner center_div">
-            <div className="card">
-              <div className="card-title">
-                <h3>Add Course</h3>
-              </div>
-              <div className="card-body">
-                <form onSubmit={this.submitCourse.bind(this, createCourse)}>
-                  <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input
-                      name="name"
-                      type="text"
-                      className="form-control"
-                      placeholder="Enter name"
-                      value={this.state.name}
-                      onChange={this.onChangeHandler}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="description">Description</label>
-                    <textarea
-                      name="description"
-                      className="form-control"
-                      rows="3"
-                      value={this.state.description}
-                      onChange={this.onChangeHandler}
-                    />
-                  </div>
-
-                  <button type="submit" className="btn btn-primary btn-block ">
-                    Save
-                  </button>
-                </form>
-              </div>
-            </div>
-          </div>
-        )}
+        {renderCreateCourse}
       </Mutation>
     );
   }
 }
+const CREATE_COURSE_MUTATION = gql`
+  mutation CreateCourse($name: String!, $description: String!) {
+    createCourse(name: $name, description: $description) {
+      id
+      name
+      description
+      isPublished
+    }
+  }
+`;
 export default AddCourse;
