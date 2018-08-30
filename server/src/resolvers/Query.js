c912cb831ce61277b48c4290e2ce308168ee31a1
Haider Malik <haidermalik504@gmail.com>
2018-08-30 17:37:26 +0500

    finsih auth

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,3 +1,4 @@
+const { getUserId } = require('../utils');
 function feed(parent, args, ctx, info) {
   return ctx.db.query.posts({ where: { isPublished: true } }, info);
 }
@@ -13,11 +14,16 @@ function courseFeed(parent, args, ctx, info) {
 function course(parent, { id }, ctx, info) {
   return ctx.db.query.course({ where: { id } }, info);
 }
+function me(parent, args, ctx, info) {
+  const id = getUserId(ctx);
+  return ctx.db.query.user({ where: { id } }, info);
+}
 
 module.exports = {
   feed,
   drafts,
   post,
   courseFeed,
-  course
+  course,
+  me
 };
