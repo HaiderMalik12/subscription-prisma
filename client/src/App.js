fad1297c1e776d48d5d720be5e4e24be822fdafa
Haider Malik <haidermalik504@gmail.com>
2018-08-31 19:05:52 +0500

    protect route

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,52 @@ import AddCourse from './components/AddCourse';
 import Header from './components/Header';
 import { Switch, Route } from 'react-router-dom';
 import EditCourse from './components/EditCourse1';
+import { AuthRoute, UnauthRoute } from 'react-router-auth';
 import Auth from './components/Auth';
+import { AUTH_TOKEN } from './utils';
 
 class App extends Component {
   render() {
+    const isAuth = localStorage.getItem(AUTH_TOKEN) ? true : false;
     return (
       <div>
         <Header />
         <Switch>
           <Route exact path="/" component={Courses} />
-          <Route exact path="/login" component={Auth} />
-          <Route exact path="/signup" component={Auth} />
-          <Route exact path="/feed" component={Courses} />
-          <Route exact path="/add" component={AddCourse} />
-          <Route exact path="/course/:id/edit" component={EditCourse} />
+          <UnauthRoute
+            exact
+            path="/login"
+            component={Auth}
+            authenticated={isAuth}
+            redirectTo="/feed"
+          />
+          <UnauthRoute
+            exact
+            path="/signup"
+            component={Auth}
+            authenticated={isAuth}
+            redirectTo="/feed"
+          />
+          <Route
+            exact
+            path="/feed"
+            component={Courses}
+            authenticated={isAuth}
+          />
+          <AuthRoute
+            exact
+            path="/add"
+            component={AddCourse}
+            redirectTo="/login"
+            authenticated={isAuth}
+          />
+          <AuthRoute
+            exact
+            path="/course/:id/edit"
+            component={EditCourse}
+            redirectTo="/login"
+            authenticated={isAuth}
+          />
         </Switch>
       </div>
     );
